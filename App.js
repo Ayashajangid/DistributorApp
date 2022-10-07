0de410b857b4fd3c0cfe86fd098ad4fa8946f489
premsuthar42 <91833039+premsuthar42@users.noreply.github.com>
2022-10-07 18:38:19 +0530

    pod install

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
-import GroupScreen from './src/screens/GroupScreen'
-
+import {Text, View} from 'react-native';
+import GroupScreen from './src/screens/GroupScreen';
 
 const App = () => {
   return (
